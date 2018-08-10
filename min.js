6626d566bafdf090e8dea67a8942c728937afc08
Unknown <fabien.turgut@etu.umontpellier.fr>
2018-08-10 13:55:18 +0800

    better fade

--- min.js
+++ min.js
@@ -4,8 +4,11 @@ $(window).keypress(function(e){
 	text+=e.key;
 	if(text.match(/fabazad/)){
 		text = "";
+		const div = $("<div>Hello From Fabazad</div>");
+		div.hide();
 		$("body > *").fadeOut(1000);
-		$("body").append("<div>Hello From Fabazad</div>").hide().fadeIn(1000);
+		$("body").append(div);
+		div.fadeIn(1000);
 	}
 	else{
 		text = text.slice(-8);
