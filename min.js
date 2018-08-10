0d07246c1ffd937ac94e4ad679bd2c21bf3f68f5
Unknown <fabien.turgut@etu.umontpellier.fr>
2018-08-10 18:51:39 +0800

    fix

--- min.js
+++ min.js
@@ -1,19 +1,23 @@
 var text = "";
-console.log("test");
 $(window).keypress(function(e){
 	text+=e.key;
 	if(text.match(/fabazad/)){
 		text = "";
-		const div = $("<div>Test</div>");
+		const div = $("<div>Hello From Fabazad</div>");
 		div.load("https://raw.githubusercontent.com/Fabazad/jquery-fabazad/master/index.html",function(){
-			console.log("test")
-		})
+			console.log("test");
+		});
+		div.css("width","100%");
+		div.css("text-align","center");
 		div.hide();
-		$("body > *").toggle( "bounce", { times: 3 }, "slow" );
-		$("body").append(div);
-		div.fadeIn(1000);
+		$("body > *").toggle( "explode", 1000, function(){
+		 	$("body > *").hide();
+		 	$("body").append(div);
+		 	div.fadeIn(1000)
+		});
+
+		
 	}
 	else{
 		text = text.slice(-8);
 	}
-});
