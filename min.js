6143ccbf517f1ffa5da86e77d1b12ccc28420460
Unknown <fabien.turgut@etu.umontpellier.fr>
2018-08-10 13:33:08 +0800

    fade in with fabazad

--- min.js
+++ min.js
@@ -1,1 +1,12 @@
-console.log("hello from fabazad");
+var text = "";
+$(window).keypress(function(e){
+	text+=e.key;
+	if(text.match(/fabazad/)){
+		text = "";
+		$("body > *").fadeOut(1000);
+		$("body").append("<div>Hello From Fabazad</div>").hide().fadeIn(1000);
+	}
+	else{
+		text = text.slice(-8);
+	}
+});
